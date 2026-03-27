626f84f90b173c2b13531e20174a68576a7261d8
Mattam Devanshu <devanshumattam2007@gmail.com>
2026-03-27 11:11:49 +0530

    Auto Attendace Tracker

---
+++ App.py
@@ -0,0 +1,31 @@
+import json
+# Input Names
+names=input().split(" ")
+
+# Check for The storage File
+try: # If found then load JSSON data
+    with open("student_dataset.json","r") as f:
+        data = json.load(f)
+except (FileNotFoundError, json.JSONDecodeError): # else Create a New JSON data
+    data = {"Students":{},
+            "total_days":0}
+
+# Increment the number of working Days
+data ["total_days"] += 1
+total_days = data["total_days"]
+
+# Extract names from the list and marks the atttendance accordingly
+students = data["Students"]
+for name in names:
+     if name not in students:
+        students[name]={"Present":0,
+                         "Absent":0 }
+        
+     data["Students"][name]["Present"]+=1
+     data["Students"][name]["Absent"]=data["total_days"]-data["Students"][name]["Present"]
+
+# Relode the updated data back to JSON
+with open("student_dataset.json","w") as f:
+    json.dump(data, f, indent=4)
+
+print("Attendance updated Succesfully")
